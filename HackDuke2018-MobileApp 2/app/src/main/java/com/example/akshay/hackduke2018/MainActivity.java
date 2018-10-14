d3a15846a106d8ce2b659c3bdad927e535a2f2c6
unknown <akshayadagale075@gmail.com>
2018-10-14 04:37:48 -0400

    AndroidPush

--- HackDuke2018-MobileApp 2/app/src/main/java/com/example/akshay/hackduke2018/MainActivity.java
+++ HackDuke2018-MobileApp 2/app/src/main/java/com/example/akshay/hackduke2018/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.akshay.hackduke2018;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Debug;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -25,22 +33,28 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "test";
     Button login,register;
     EditText name,zip,job,number,skill,region;
+    TextView title;
+    int length=0,lenghtRegion=0,lengthJob=0;
+    Boolean checked = false;
     private String remoteIP="http://c3ee4bf3.ngrok.io";
+    Toolbar mTopToolbar;
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       /* mTopToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mTopToolbar);*/
+        relativeLayout = findViewById(R.id.relativelayout);
         name = findViewById(R.id.editTextName);
-        zip = findViewById(R.id.editTextZip);
         number=findViewById(R.id.editTextNumber);
         skill= findViewById(R.id.editTextSkill);
-        job = findViewById(R.id.editTextJob);
+        zip = findViewById(R.id.editTextZip);
         region =findViewById(R.id.editTextRegion1);
-       // password = findViewById(R.id.editTextPassword);
-
-     //   login = findViewById(R.id.buttonLogin);
+        job = findViewById(R.id.editTextJob);
+        title = findViewById(R.id.title);
         register = findViewById(R.id.buttonCreate);
 
 
@@ -48,13 +62,13 @@ public class MainActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
- String name1,zip1,number1,skill1,region1,job1;
- name1 = name.getText().toString();
- zip1 = zip.getText().toString();
- number1= "+1"+ number.getText().toString();
- skill1= skill.getText().toString();
- region1= region.getText().toString();
- job1= job.getText().toString();
+        String name1,zip1,number1,skill1,region1,job1;
+        name1 = name.getText().toString();
+        zip1 = zip.getText().toString();
+        number1= "+1"+ number.getText().toString();
+        skill1= skill.getText().toString();
+        region1= region.getText().toString();
+        job1= job.getText().toString();
 
                 final OkHttpClient client = new OkHttpClient();
                 RequestBody formBody = new FormBody.Builder()
@@ -86,5 +100,152 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.toString().contains("@")){
+                    number.setVisibility(View.VISIBLE);
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        number.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+               if(charSequence.length() == 10)
+                   length = Integer.parseInt(String.valueOf(charSequence.length()));
+                skill.setVisibility(View.VISIBLE);
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(length < 10)
+                    skill.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        skill.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(skill.length()>=4){
+                    zip.setVisibility(View.VISIBLE);
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        zip.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.length()==5) {
+                    region.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        region.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.length()>=2){
+                    lenghtRegion = Integer.parseInt(String.valueOf(charSequence.length()));
+                    job.setVisibility(View.VISIBLE);
+                    checked = true;
+
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(lenghtRegion<2){
+                    job.setVisibility(View.INVISIBLE);
+                    checked = false;
+                }
+            }
+        });
+
+        job.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                lengthJob = Integer.parseInt(String.valueOf(charSequence.length()));
+                if(charSequence.length()>=2 && checked == true){
+                    register.setVisibility(View.VISIBLE);
+                }else if(charSequence.length()<1){
+                    register.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(lengthJob<2){
+                    register.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
+
+
+  /*  public void onClick(View v)
+    {
+        if (v == name)
+        {
+            if (name.equals("@"))
+            {
+
+            }
+            else
+            {
+                number.setVisibility(View.VISIBLE);
+            }
+        }
+
+    }*/
 }
